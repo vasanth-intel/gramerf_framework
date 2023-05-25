b15972b85ebdae80f7a709c86652e7a428c32318
Jinen Gandhi <jinen.gandhi@intel.com>
2023-05-25 10:28:44 +0530

    Enabling MariaDB and refactoring MySQL to use the same libs

--- tests/test_mysql_performance.py
+++ tests/test_mysql_performance.py
@@ -6,86 +6,16 @@ import sys
 from common.config_files.constants import *
 from common.libs.gramerf_wrapper import run_test
 from common.libs import utils
+from docker_benchmarking.workloads.db_workloads_utils import *
 
 yaml_file_name = "mysql_performance_tests.yaml"
 tests_yaml_path = os.path.join(os.getcwd(), 'data', yaml_file_name)
-
-
-def init_db(workload_name):
-    docker_output = ''
-    output = None
-    init_result = False
-    timeout = time.time() + 60*10
-    try:
-        os.makedirs(eval(workload_name.upper()+"_TESTDB_PATH"), exist_ok=True)
-        process = subprocess.Popen(eval(workload_name.upper()+"_INIT_DB_CMD"), cwd=CURATED_APPS_PATH, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
-        print(f"Initializing {workload_name.upper()} DB")
-        while True:
-            if process.poll() is not None and output == '':
-                break
-            output = process.stderr.readline()
-            print(output)
-            if output:
-                docker_output += output
-                if (docker_output.count(eval(workload_name.upper()+"_TESTDB_VERIFY")) == 2):
-                    print(f"{workload_name.upper()} DB is initialized\n")
-                    init_result = True
-                    break
-                elif time.time() > timeout:
-                    break
-    finally:
-        process.stdout.close()
-        process.stderr.close()
-        utils.kill(process.pid)
-    if init_result:
-        utils.exec_shell_cmd(STOP_TEST_DB_CMD)
-        utils.exec_shell_cmd("sudo rm -rf /var/run/test_db_plain && \
-                            sudo mkdir -p /var/run/test_db_plain")
-        if "mariadb" in workload_name:
-            utils.exec_shell_cmd(MARIADB_CHMOD)
-            utils.exec_shell_cmd(COPY_MARIADB_TESTDB)
-        else:
-            if os.environ["encryption"] != "1" and os.environ["tmpfs"] == "1":
-                utils.exec_shell_cmd(f"sudo rm -rf {PLAIN_DB_TMPFS_PATH} && \
-                            sudo mkdir -p {PLAIN_DB_TMPFS_PATH}")
-                print(f"Copying Test DB to {PLAIN_DB_TMPFS_PATH}")
-                utils.exec_shell_cmd(f"sudo cp -rf {MYSQL_TESTDB_PATH}/* {PLAIN_DB_TMPFS_PATH}")
-            elif os.environ["encryption"] != "1" and os.environ["tmpfs"] != "1":
-                utils.exec_shell_cmd(f"sudo rm -rf {PLAIN_DB_REGFS_PATH} && \
-                            sudo mkdir -p {PLAIN_DB_REGFS_PATH}")
-                print(f"Copying Test DB to {PLAIN_DB_REGFS_PATH}")
-                utils.exec_shell_cmd(f"sudo cp -rf {MYSQL_TESTDB_PATH}/* {PLAIN_DB_REGFS_PATH}")
-            elif os.environ["encryption"] == "1" and os.environ["tmpfs"] != "1":
-                utils.exec_shell_cmd(f"sudo rm -rf {ENCRYPTED_DB_REGFS_PATH} && \
-                            sudo mkdir -p {ENCRYPTED_DB_REGFS_PATH}")
-                #print(f"Copying Test DB to {ENCRYPTED_DB_REGFS_PATH}")
-                #utils.exec_shell_cmd(f"sudo cp -rf {MYSQL_TESTDB_PATH}/* {ENCRYPTED_DB_REGFS_PATH}")
-            elif os.environ["encryption"] == "1" and os.environ["tmpfs"] == "1":
-                utils.exec_shell_cmd(f"sudo rm -rf {ENCRYPTED_DB_TMPFS_PATH} && \
-                            sudo mkdir -p {ENCRYPTED_DB_TMPFS_PATH}")
-                #print(f"Copying Test DB to {ENCRYPTED_DB_TMPFS_PATH}")
-                #utils.exec_shell_cmd(f"sudo cp -rf {MYSQL_TESTDB_PATH}/* {ENCRYPTED_DB_TMPFS_PATH}")
-    return init_result
-
-
-def encrypt_db(workload_name):
-    output = utils.popen_subprocess(eval(workload_name.upper()+"_TEST_ENCRYPTION_KEY"), CURATED_APPS_PATH)
-    if os.environ["encryption"] == "1":
-        if os.environ["tmpfs"] == "1":
-            output = utils.popen_subprocess(CLEANUP_ENCRYPTED_DB_TMPFS, CURATED_APPS_PATH)
-            encryption_output = utils.popen_subprocess(eval(workload_name.upper()+"_ENCRYPT_DB_TMPFS_CMD"), CURATED_APPS_PATH)
-        else:
-            output = utils.popen_subprocess(CLEANUP_ENCRYPTED_DB_REGFS, CURATED_APPS_PATH)
-            encryption_output = utils.popen_subprocess(eval(workload_name.upper()+"_ENCRYPT_DB_REGFS_CMD"), CURATED_APPS_PATH)
 
 @pytest.fixture(scope="session")
 def execute_workload_setup():
     init_result = init_db("mysql")
     if init_result == False:
         sys.exit("DB initialization failed")
-    # Generate encryption key and encrypt MySql/MariaDB database.
-    encrypt_db("mysql")
-
 
 @pytest.mark.usefixtures("execute_workload_setup")
 @pytest.mark.usefixtures("gramerf_setup")
